9b5e44075f81e44c0b2d555208a9e635a6eb2c8c
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2023-04-02 11:31:52 +0530

    create question API done

--- API/question.js
+++ API/question.js
@@ -24,5 +24,66 @@ Router.post("/create", async(req,res)=>{
 
 });
 
+/*
+route      ==> /quiz
+method     ==> POST
+Des        ==> get quiz based on the _id
+params     ==> _id
+Access     ==> public
+*/
+
+Router.get("/quiz/:_id",async(req,res)=>{
+    try {
+        const {_id} = req.params;
+        const findQuiz = await QuizModel.findOne({_id});
+        if(!findQuiz || findQuiz.length === 0){
+            return res.status(201).json({message : "quiz not found"});
+        }
+        return res.status(200).json({findQuiz});
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+        
+    }
+
+});
+
+
+/*
+route      ==> /question
+method     ==> POST
+Des        ==> create question by using user
+params     ==> _id
+Access     ==> public
+*/
+
+Router.post("/question/:_id", async(req,res)=>{
+    try {
+        const {question} = req.body;
+        const {_id} = req.params;
+        const isValidOrNotID = await QuizModel.findById(_id);
+        if(!isValidOrNotID || isValidOrNotID.length === 0){
+            return res.status(500).json({message : "Wrong id"});
+        }
+        const findQuiz = await QuestionModel.findOne({quiz : _id});
+        if(!findQuiz || findQuiz.length === 0){
+            const newQuestionSchema = {
+                quiz : _id,
+                question : question
+            }
+            const questionData = await QuestionModel.create(newQuestionSchema);
+            return res.status(200).json({questionData});
+        }
+        const questionData = await QuestionModel.findOneAndUpdate({quiz : _id},{
+            $push : {
+                question : question
+            }
+        }, {new : true});
+        return res.status(200).json({questionData});
+        
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
 
 export default Router;
--- Database/question.js
+++ Database/question.js
@@ -1,54 +1,56 @@
 import mongoose from 'mongoose';
 
 const QuestionSchema = new mongoose.Schema({
-    questionName: {
-        type : String,
-        required : true
-    },
-    quiz : {
-        type : mongoose.Types.ObjectId,
-        ref : "quiz",
-    },
-    option1: {
-        type : String,
-        required : true
-    },
-    option2: {
-        type : String,
-        required : true
-    },
-    option3: {
-        type : String,
-        required : true
-    },
-    option4: {
-        type : String,
-        required : true
-    },
-    isMultipleAnswer: {
-        type : Boolean,
-        required : true,
-        default : false
-    },
-    answer: {
-        type : String,
-    },
-    answer1: {
-        type : String,
-    },
-    answer2: {
-        type : String,
-    },
-    answer3: {
-        type : String,
-    },
-    answer4: {
-        type : String,
-    },
-    marks: {
-        type : String,
-    },
-},
-{timestamps : true});
 
-export const QuestionModel = mongoose.model("question",QuestionSchema);
+    quiz: {
+        type: mongoose.Types.ObjectId,
+        ref: "quiz",
+    },
+    question: [{
+        questionName: {
+            type: String,
+            required: true
+        },
+        option1: {
+            type: String,
+            required: true
+        },
+        option2: {
+            type: String,
+            required: true
+        },
+        option3: {
+            type: String,
+            required: true
+        },
+        option4: {
+            type: String,
+            required: true
+        },
+        isMultipleAnswer: {
+            type: Boolean,
+            default: false
+        },
+        answer: {
+            type: String,
+        },
+        answer1: {
+            type: String,
+        },
+        answer2: {
+            type: String,
+        },
+        answer3: {
+            type: String,
+        },
+        answer4: {
+            type: String,
+        },
+        marks: {
+            type: String,
+        },
+    }]
+},
+    { timestamps: true });
+
+export const QuestionModel = mongoose.model("question", QuestionSchema);
